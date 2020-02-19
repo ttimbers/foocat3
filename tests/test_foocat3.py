b8b42d5eed250c207190acbef4bdc71a732459f2
ttimbers <tiffany.timbers@stat.ubc.ca>
2020-02-18 16:37:39 -0800

    BREAKING CHANGE: code is now in the repo :woman_facepalming

--- tests/test_foocat3.py
+++ tests/test_foocat3.py
@@ -1,3 +1,11 @@
 import pytest
 
 from foocat3 import foocat3
+
+import pandas as pd
+
+def test_catbind():
+  a = pd.Categorical(["character", "hits", "your", "eyeballs"])
+  b = pd.Categorical(["but", "integer", "where it", "counts"])
+  assert ((foocat3.catbind(a, b)).codes == [1, 4, 7, 3, 0, 5, 6, 2]).all()
+  assert ((foocat3.catbind(a, b)).categories == ["but", "character", "counts", "eyeballs", "hits", "integer", "where it", "your"]).all()
